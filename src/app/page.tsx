49f7f61905809ef368b37219f63180e3f804029b
noor2607 <larebnoor4@gmail.com>
2024-11-20 23:00:04 +0500

    Your commit message here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Hero from "@/components/Hero"
-
+import About from "./About/page"
+import Services from "./Services/page"
+import Contactus from "./Contact-us/page"
 export default function Home (){
   return(
     <div>
       <div>
         <Hero/>
+        <About/>
+        <Services/>
+        <Contactus/>
+
       </div>
     </div>
   )
